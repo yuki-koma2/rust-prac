7e0866cdebf4edc991460988d4da7266aaf3f98a
yuki komatsu <yuki.k.lion@gmail.com>
2025-06-10 01:08:12 +0900

    Add wasm calculator package and update CI

---
+++ wasm-calculator/calc/src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add(2, 2), 4);
+    }
+}
